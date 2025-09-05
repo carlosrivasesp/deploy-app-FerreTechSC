e7353fbed18ff07c0679982e4efb10e993e146d3
AndresBaldeonMora <andres.baldeon2024@gmail.com>
2025-09-04 21:30:37 -0500

    .

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CartItem } from '../../models/carrito'; // Importar la interfaz
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
     selector: 'app-carrito',
@@ -7,7 +8,7 @@ import { CartItem } from '../../models/carrito'; // Importar la interfaz
     templateUrl: './carrito.component.html',
     styleUrls: ['./carrito.component.css']
 })
-export class CarritoComponent implements OnInit {
+export class CarritoComponent implements OnInit, OnDestroy {
     cartItems: CartItem[] = [
         { nombre: 'Producto 1', precio: 20.00, cantidad: 1 },
         { nombre: 'Producto 2', precio: 15.00, cantidad: 1 }
@@ -15,14 +16,26 @@ export class CarritoComponent implements OnInit {
 
     totalPrice = 0;
 
+    // Inyectar el servicio SidebarService para manejar la visibilidad del sidebar
+    constructor(private sidebarService: SidebarService) { }
+
     ngOnInit(): void {
+        // Ocultar el sidebar cuando el componente Carrito se cargue
+        this.sidebarService.hideSidebar();
         this.updateTotal();
     }
 
+    ngOnDestroy(): void {
+        // Mostrar el sidebar cuando el componente Carrito se destruya
+        this.sidebarService.showSidebar();
+    }
+
+    // Método para actualizar el precio total
     updateTotal(): void {
         this.totalPrice = this.cartItems.reduce((total, item) => total + (item.precio * item.cantidad), 0);
     }
 
+    // Método para eliminar un item del carrito
     removeItem(item: CartItem): void { // Definir el tipo del parámetro 'item'
         const index = this.cartItems.indexOf(item);
         if (index > -1) {
